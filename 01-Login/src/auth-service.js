081defcf1bc674aeab91c58ff5e804e76af0a51b
João Angelo <jmangelo@outlook.com>
2018-09-04 14:16:47 +0100

    Remove userinfo audience from login steps

--- 01-Login/src/auth-service.js
+++ 01-Login/src/auth-service.js
@@ -10,7 +10,6 @@ export class AuthService {
     domain: AUTH_CONFIG.domain,
     clientID: AUTH_CONFIG.clientId,
     redirectUri: AUTH_CONFIG.callbackUrl,
-    audience: `https://${AUTH_CONFIG.domain}/userinfo`,
     responseType: 'token id_token',
     scope: 'openid'
   });
